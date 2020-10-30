d83cf456b57a9da28a9cea1a5780f4551f7205ec
it63070029 <73680368+it63070029@users.noreply.github.com>
2020-10-30 11:03:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<html>
+  <head>
+    <title>LAB ITF</title>
+   </head>
+  <body>
+    <h1>NAME : Chanika Preechaphonlakul</h1>
+    <h2>NICKNAME : Parn</h2>
+    <p>My Picture</p>
+    <img scr="https://miro.medium.com/max/700/1*lS9ZqdEGZrRiTcL1JUgt9w.jpeg">
+    <img scr="https://miro.medium.com/max/700/1*bw6zaI7HRtrF-8ctZMd2xg.jpeg">
+    <a href="https://www.google.co.th/">Google</a>
+    <a href="https://www.youtube.com/">Youtube</a>
+    <h3>Joke</h3>
+    <ul>
+      <li>Joke</li>
+      <li>Joke</li>
+      <li>Joke</li>
+    </ul>
+  </body>
+</html>
