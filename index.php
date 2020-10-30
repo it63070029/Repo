63e07e1754c06125d07edeba8003266dc1ef465e
it63070029 <73680368+it63070029@users.noreply.github.com>
2020-10-30 11:06:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <h1>NAME : Chanika Preechaphonlakul</h1>
     <h2>NICKNAME : Parn</h2>
     <p>My Picture</p>
-    <img scr="https://miro.medium.com/max/700/1*lS9ZqdEGZrRiTcL1JUgt9w.jpeg">
-    <img scr="https://miro.medium.com/max/700/1*bw6zaI7HRtrF-8ctZMd2xg.jpeg">
+    <img scr="cat.jpg">
+    <img scr="cat1.jpg">
     <a href="https://www.google.co.th/">Google</a>
     <a href="https://www.youtube.com/">Youtube</a>
     <h3>Joke</h3>
