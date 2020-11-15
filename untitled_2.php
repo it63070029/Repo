a25caaf722b8f714f7da5cf5ca82e2d13a9ee039
it63070029 <73680368+it63070029@users.noreply.github.com>
2020-11-16 00:47:48 +0700

    Add files via upload

---
+++ untitled_2.php
@@ -0,0 +1,15 @@
+<form action="/show.php">
+  <div class="form-group">
+    <label for="Name">Name:</label>
+    <input type="Name" class="form-control" placeholder="Enter Name" id="Name">
+  </div>
+  <div class="form-group">
+    <label for="Comment">Comment:</label>
+    <input type="Comment" class="form-control" placeholder="Enter Comment" id="Comment">
+  </div>
+  <div class="form-group">
+    <label for="Link">Link:</label>
+    <input type="Link" class="form-control" placeholder="Enter Link" id="Link">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
