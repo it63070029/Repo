bc8311a558c92fef909fb67cd3c0790609851b1f
it63070029 <73680368+it63070029@users.noreply.github.com>
2020-11-18 17:20:19 +0700

    Update show.php

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'chanika.mysql.database.azure.com', 'it63070029@chanika', 'chanika00_', 'itflab', 3306);
+mysqli_real_connect($conn, 'chanika.mysql.database.azure.com', 'it63070029@chanika', 'chanika00_', 'itf_lab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
@@ -14,9 +14,9 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <table width="600" border="1">
   <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
+    <th width="100" style="background-color:#d2e7d1;"> <div align="center">Name</div></th>
+    <th width="350" style="background-color:#ddecdc;"> <div align="center">Comment </div></th>
+    <th width="150" style="background-color:#f1f8f1;"> <div align="center">Link </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
