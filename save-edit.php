6860ad93038dac62445f0314a1c4851d9f28acc0
it63070029 <73680368+it63070029@users.noreply.github.com>
2020-11-19 20:57:53 +0700

    Add files via upload

---
+++ save-edit.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'chanika.mysql.database.azure.com', 'it63070029@chanika', 'chanika00_', 'itf_lab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+$id = $_POST['id'];
+
+
+$sql = "UPDATE guestbook SET name = '$name', comment = '$comment', link = '$link', id ='$id';
+$query = mysqli_query($conn, $sql);
+
+
+if ($query) {
+    header('location: show.php');
+  } else {
+    echo 'Can not Update';
+  }
+?>
